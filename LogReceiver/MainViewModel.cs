85c75899153de03753ace52656db90045cba6ca4
Ben Taylor <bentaylortheonly@gmail.com>
2024-12-19 09:52:35 +0000

    add logger options

--- LogReceiver/MainViewModel.cs
+++ LogReceiver/MainViewModel.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
+using System.Linq;
+using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Input;
 using Prism.Commands;
@@ -11,16 +14,35 @@ namespace LogReceiver
     public class MainViewModel : INotifyPropertyChanged
     {
         private readonly List<MessageData> eventList;
+        
         private MessageData selectedMessage;
         private bool isPaused;
+        private bool defaultLoggerOption = true;
 
         public ICommand ClearCommand { get; }
         public ICommand ClearTreeCommand { get; }
         public ICommand TogglePauseCommand { get; }
 
+        private readonly Dictionary<string, LoggerOption> loggerOptionsDictionary;
+        private readonly List<LoggerOption> loggerOptionsList;
+        public ListCollectionView LoggerOptions { get; }
+
         protected void BeginInvokePropertyChanged(string propertyName)
         {
             PropertyChanged?.BeginInvoke(this, new PropertyChangedEventArgs(propertyName), null, null);
+        }
+
+        public bool DefaultLoggerOption
+        {
+            get => defaultLoggerOption;
+            set
+            {
+                if (defaultLoggerOption != value)
+                {
+                    defaultLoggerOption = value;
+                    BeginInvokePropertyChanged(nameof(DefaultLoggerOption));
+                }
+            }
         }
 
         public bool IsPaused
@@ -52,32 +74,46 @@ namespace LogReceiver
         public string TogglePauseCommandContent => IsPaused ? "Resume" : "Pause";
 
         public ListCollectionView Events { get; }
-
         
         public MainViewModel()
         {
             App.EventAggregator.Value.GetEvent<MessageEvent>().Subscribe(AddMessage, ThreadOption.UIThread);
-            App.EventAggregator.Value.GetEvent<InvalidateFilterCacheEvent>().Subscribe(InvalidateFilterCache, ThreadOption.UIThread);
             eventList = new List<MessageData>();
             Events = new ListCollectionView(eventList);
+            loggerOptionsList = new List<LoggerOption>();
+            LoggerOptions = new ListCollectionView(loggerOptionsList);
+            loggerOptionsDictionary = new Dictionary<string, LoggerOption>();
             ClearCommand = new DelegateCommand(Clear);
             TogglePauseCommand = new DelegateCommand(TogglePause);
+            AllOnCommand = new DelegateCommand(AllOn);
+            AllOffCommand = new DelegateCommand(AllOff);
+        }
+
+        public DelegateCommand AllOffCommand { get; set; }
+
+        public DelegateCommand AllOnCommand { get; set; }
+
+        private void AllOn()
+        {
+            foreach (var logger in loggerOptionsDictionary)
+            {
+                logger.Value.IsOn = true;
+            }
+        }
+
+        private void AllOff()
+        {
+            foreach (var logger in loggerOptionsDictionary)
+            {
+                logger.Value.IsOn = false;
+            }
         }
 
         private void TogglePause()
         {
             IsPaused = !IsPaused;
         }
-
-        private void InvalidateFilterCache(InvalidateFilterCacheEventArgs args)
-        {
-            foreach(var affectedLogger in args.AffectedLoggers)
-            {
-                filterCache.AddOrUpdate(affectedLogger, args.Value, (s, b) => args.Value);
-            }
-            Events.Refresh();
-        }
-
+        
         private void Clear()
         {
             eventList.Clear();
@@ -85,15 +121,27 @@ namespace LogReceiver
         }
 
         private static readonly ConcurrentDictionary<string, bool> filterCache = new ConcurrentDictionary<string, bool>();
-
         public event PropertyChangedEventHandler PropertyChanged;
 
 
-        public void AddMessage(MessageData msgs)
+        public void AddMessage(MessageData msg)
         {
             if (!IsPaused)
             {
-                eventList.Add(msgs);
+                LoggerOption loggerOption;
+                if (!loggerOptionsDictionary.TryGetValue(msg.Logger, out loggerOption))
+                {
+                    loggerOption = new LoggerOption(msg.Logger) { IsOn = DefaultLoggerOption };
+                    loggerOptionsDictionary.Add(msg.Logger, loggerOption);
+                    loggerOptionsList.Clear();
+                    loggerOptionsList.AddRange(loggerOptionsDictionary.OrderBy(d => d.Key).Select(d => d.Value));
+                    LoggerOptions.Refresh();
+                }
+
+                if (loggerOption.IsOn)
+                {
+                    eventList.Add(msg);
+                }
 
                 if (eventList.Count > 5000)
                 {
